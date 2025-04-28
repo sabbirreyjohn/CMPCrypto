8c1442c10fce4f4fb4aed80f69dd231b768dd375
Rey <rey.sabbir@gmail.com>
2025-04-28 15:48:54 +0800

    Added library and theme

--- composeApp/src/commonMain/kotlin/xyz/androidrey/cmp/App.kt
+++ composeApp/src/commonMain/kotlin/xyz/androidrey/cmp/App.kt
@@ -2,10 +2,13 @@ package xyz.androidrey.cmp
 
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import xyz.androidrey.cmp.theme.CmpTheme
 
 @Composable
 @Preview
 fun App() {
+    CmpTheme {
 
+    }
 
 }
